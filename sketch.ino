9845de8cdf670b4a2f4654a19f811fe3f33e7c7a
Jack Doan <me@jackdoan.com>
2014-12-02 01:41:20 -0600

    added interrupt functionality

--- sketch.ino
+++ sketch.ino
@@ -10,19 +10,25 @@
  
  */
 
-const int analogInPin = P1_0;  // Analog input pin
-const int greenLED = P1_6;
+const int infraLED = P1_0;
+const int analogInPin = P1_4;  // Analog input pin
 const long baud = 115200;
 const int sensitivity = 75; // integer from 0-1024, where 0 is 0 volts across the pullup, and 1024 3.3 volts
 
+volatile bool infraState = 0;
 int sensorValue = 0;  
 int counter = 0;
 int lightStatus = 0;
 
+void infraOnOff();
+
 void setup() {
   Serial.begin(baud);
-  pinMode(greenLED, OUTPUT);
-  digitalWrite(greenLED, 0); 
+  pinMode(infraLED, OUTPUT);
+  pinMode(PUSH2, INPUT_PULLUP);
+  pinMode(GREEN_LED, OUTPUT);
+  digitalWrite(GREEN_LED, 0);
+  attachInterrupt(PUSH2, infraOnOff, FALLING); 
 }
 
 void loop() {
@@ -32,28 +38,37 @@ void loop() {
     delay(20); //debounce it a little
     if (sensorValue >= 75) {
       if (lightStatus == 0) {
-        digitalWrite(greenLED, 1);
+        digitalWrite(GREEN_LED, 1);
         lightStatus = 1;
       }
       else {
-        digitalWrite(greenLED, 0);
+        digitalWrite(GREEN_LED, 0);
         lightStatus = 0;
       }
     }
   }         
 
   // print the results to the serial monitor if it is significant:
+  /*  DEBUG
   if (sensorValue >= 10) {
     counter = sensorValue / 5;
     while (counter != 0) {
       Serial.print("#");
       counter--;
     }
+    
+    Serial.print(" - ");
+    Serial.print(sensorValue);
     Serial.println("");
-    Serial.println(sensorValue);
   }
-
+  */
   // wait 10 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   delay(10);                     
 }
+
+void infraOnOff() {
+  infraState = !infraState;
+  digitalWrite(infraLED, infraState);
+}
+  
